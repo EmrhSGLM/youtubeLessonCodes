016075177ab0806f377eb596654d46b7deafdd7a
EmrhSGLM <emrh.sglm7168@gmail.com>
2022-11-01 23:56:40 +0300

    Selenium-2

---
+++ seleniumFirstProject1/src/test/java/FirstTest_Class.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest_Class {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\project\\chromeDriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https:\\www.amazon.com");
+
+        Thread.sleep(3000);
+
+        driver.close();
+
+
+
+    }
+
+
+}
