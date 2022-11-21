fe8c917826def228a2a05da5e183e1cf6a809f1c
EmrhSGLM <emrh.sglm7168@gmail.com>
2022-11-22 02:22:33 +0300

    java_q8

---
+++ Java_Interview_Questions/src/Question_08.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Question_08 {
+
+    public static void main(String[] args) {
+        /*
+
+		Kullanıcıdan 2 String girmesini isteyin. String'lerin buyuk-kucuk harf duyarliligi olmadan
+		karakterleri ve karakter sayıları aynıysa console girilen karakterler "Anagram" ,
+		aksi takdirde console "Anagram Değil" yazdırın.
+		Ornek : "Pide" ve"EDİP" anagram ifadelerdir..
+		        "Mary" and "army" and "RAMY" are Anagrams
+
+		 */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. String ifadeyi giriniz : ");
+        String str1 = scan.next();
+        System.out.print("2. String ifadeyi giriniz : ");
+        String str2 = scan.next();
+        Boolean result = true;
+
+        if(str1.length() == str2.length()){
+
+            for(int i=0 ; i<str1.length() ; i++){
+
+                if(str1.toLowerCase().contains(str2.toLowerCase().substring(i,i+1))){
+                    continue;
+                }else {
+                    result = false;
+                    break;
+                }
+            }
+
+        }else{
+            result = false;
+        }
+
+        if(result){
+            System.out.println("Anagram");
+        }else{
+            System.out.println("Anagram Degil");
+        }
+
+
+
+
+
+    }
+}
