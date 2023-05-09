ec4928a2af8cab196305fc7af5619569955f9ab4
EmrhSGLM <emrh.sglm7168@gmail.com>
2023-05-09 07:20:07 +0300

    Assigment Operators

---
+++ JavaDersleri/src/lesson_15/AssignmentOperators.java
@@ -0,0 +1,24 @@
+package lesson_15;
+
+public class AssignmentOperators {
+
+    public static void main(String[] args) {
+        // Assigment(Atama) Operatoru
+        //  =
+        // +=
+        // -=
+        // *=
+        // /=
+        // %=
+        int sayi = 20;
+        //sayi += 5; // sayi = sayi + 5;
+        //sayi -= 10; // sayi = sayi - 10;
+        //sayi *= 2; // sayi = sayi * 2;
+        //sayi /= 2; // sayi = sayi / 2;
+        //sayi %= 2; // sayi = sayi % 2;
+        sayi %= 3; // sayi = sayi % 3;
+        System.out.println("sayi = " + sayi);
+
+
+    }
+}
